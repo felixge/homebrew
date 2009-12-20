b4d3dc2bfd489469947142b2673acb5386030da4
Felix Geisendörfer <felix@debuggable.com>
2009-12-20 13:47:19 +0100

    Added node head version

--- Library/Formula/node.rb
+++ Library/Formula/node.rb
@@ -2,6 +2,7 @@ require 'formula'
 
 class Node <Formula
   url 'http://s3.amazonaws.com/four.livejournal/20091206/node-v0.1.21.tar.gz'
+  head 'git://github.com/ry/node.git'
   homepage 'http://nodejs.org/'
   md5 'c72b29a803d9bb3aed0fce5245d8b03b'
 
